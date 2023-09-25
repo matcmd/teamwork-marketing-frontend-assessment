38e674bfced5c2a754fcff2be36e40b8693448d3
Mattia <mattia@Mattias-MacBook-Air.local>
2023-09-25 17:42:07 +0200

    Update project config and add base styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,23 +2,43 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Teamwork Marketing Front-End Assessment',
+    titleTemplate: 'Teamwork Marketing Front-End Assessment | %s',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
+      amp: true
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Teamwork Marketing Front-End Assessment' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=swap' }
+    ],
+    script: [
+      {
+        src: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js',
+        async: true,
+        defer: true
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/assets/styles/main.scss'
+  ],
+
+  styleResources: {
+    scss: ['~/assets/styles/variables.scss']
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    '~/plugins/vue-placeholders.js'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -26,12 +46,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/svg'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {},
+  
+  // Disable loading bar
+  loading: false
 }

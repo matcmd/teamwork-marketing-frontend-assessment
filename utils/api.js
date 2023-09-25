4455eb963dfb0a05467d9af0a6747907968dfa3f
Mattia <mattia@Mattias-MacBook-Air.local>
2023-09-25 17:40:01 +0200

    Add utils/api and utils/helpers

---
+++ utils/api.js
@@ -0,0 +1,16 @@
+import { logError } from '@/utils/helpers'
+
+const challengesURL = 'https://eoyge3duj7xtdqd.m.pipedream.net/'
+
+const fetchChallenges = async () => {
+    try {
+        const response = await fetch(challengesURL)
+        return response.json()
+    } catch (error) {
+        logError(error)
+    }
+}
+
+export {
+    fetchChallenges
+}
---
+++ utils/helpers.js
@@ -0,0 +1,16 @@
+const logError = (error) => {
+    console.error(error) // eslint-disable-line no-console
+}
+
+const logWarning = (warning) => {
+    console.warning(warning) // eslint-disable-line no-console
+}
+const logInfo = (info) => {
+    console.log(info) // eslint-disable-line no-console
+}
+
+export {
+    logError,
+    logInfo,
+    logWarning
+}
